23ddee5e3b7b7c253e310efe9c833d7950bd7396
WangTao <27059876@qq.com>
2022-03-20 21:18:14 +0800

    加上了坐标显示

--- detect.py
+++ detect.py
@@ -149,7 +149,7 @@ def detect(save_img=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp3/weights/best.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default=r'VOCdevkit\images\val\IMG_20220320_120444.jpg', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str, default=r'VOCdevkit\images\val\IMG_20220320_115956.jpg', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
